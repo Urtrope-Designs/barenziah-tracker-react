7c4cf7f7aded349051d5da9d82f68a523ab33bbd
Sean <s.cunningham777@gmail.com>
2019-08-18 22:21:40 -0500

    #2 fix bug with list index 0

--- src/store/checklists/reducer.ts
+++ src/store/checklists/reducer.ts
@@ -2,14 +2,14 @@ import { TOGGLE_STONE_FOUND_STATUS, UPDATE_CHECKLIST_NAME } from "./actions";
 import { Checklist } from "./types";
 
 const defaultState: Checklist = {
-    checklistName: '',
+    checklistName: 'Stone Locations',
     foundStoneIds: []
 }
 
 export function currentChecklist(state: Checklist = defaultState, action: any) {
     switch (action.type) {
         case TOGGLE_STONE_FOUND_STATUS:
-            const newFoundStoneIds = state.foundStoneIds.find(i => i === action.stoneId) ? state.foundStoneIds.filter(i => i !== action.stoneId) : [...state.foundStoneIds, action.stoneId];
+            const newFoundStoneIds = state.foundStoneIds.some(i => i === action.stoneId) ? state.foundStoneIds.filter(i => i !== action.stoneId) : [...state.foundStoneIds, action.stoneId];
             console.log('updated foundStoneIds: ' + newFoundStoneIds);
             return {...state, foundStoneIds: newFoundStoneIds};
         case UPDATE_CHECKLIST_NAME:

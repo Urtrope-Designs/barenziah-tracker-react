6f5297aa2bec0b6e33c6b767acd315c3be395ec4
Sean <s.cunningham777@gmail.com>
2019-08-22 21:36:34 -0500

    Close #3

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,10 +1,11 @@
-import { IonButtons, IonContent, IonHeader, IonItem, IonList, IonMenuButton, IonTitle, IonToolbar, IonCheckbox, IonLabel, IonInput } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonItem, IonList, IonMenuButton, IonTitle, IonToolbar, IonCheckbox, IonLabel, IonInput, IonButton } from '@ionic/react';
 import React from 'react';
 
 import { connect } from 'react-redux';
 import { STONE_LIST } from '../utils/stone-list';
 import { Checklist } from '../store/checklists/types';
 import { toggleStoneFoundStatus, updateChecklistName } from '../store/checklists/actions';
+import './List.css';
 
 interface PageProps {
   currentChecklist: Checklist;
@@ -12,31 +13,72 @@ interface PageProps {
   onUpdateName: Function;
 };
 
+interface PageState {
+  editValue: string;
+}
+
 interface ListProps {
   currentChecklist: Checklist;
   onItemClick: Function;
 };
 
-const ListPage: React.SFC<PageProps> = ({currentChecklist, onItemClick, onUpdateName}) => {
-  return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>
-            <IonInput value={currentChecklist.checklistName} onIonBlur={event => onUpdateName((event.target as HTMLInputElement).value)}></IonInput>
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
+class ListPage extends React.Component<PageProps, PageState> {
+  constructor(props: PageProps) {
+    super(props);
+    this.state = {
+      editValue: props.currentChecklist.checklistName,
+    };
+  }
 
-      <IonContent>
-        <ListItems currentChecklist={currentChecklist} onItemClick={(i: number) => onItemClick(i)}/>
-      </IonContent>
-    </>
-  );
-};
+  handleInputChange = (event: CustomEvent) => {
+    this.setState({editValue: (event.target as HTMLInputElement).value})
+  }
+  handleInputCancel = () => {
+    this.setState({
+      editValue: this.props.currentChecklist.checklistName,
+    });
+  }
+  handleSaveClick = () => {
+    this.props.onUpdateName(this.state.editValue);
+  }
+
+  render () {
+    return (
+      <>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton />
+            </IonButtons>
+            <IonTitle>
+              <IonInput
+                value={this.state.editValue}
+                onIonChange={this.handleInputChange}
+              ></IonInput>
+            </IonTitle>
+            <IonButtons slot="end">
+              <IonButton
+                fill="clear"
+                disabled={this.state.editValue === this.props.currentChecklist.checklistName}
+                onClick={this.handleInputCancel}
+              >Undo</IonButton>
+              <IonButton
+                fill="clear"
+                disabled={this.state.editValue === this.props.currentChecklist.checklistName}
+                onClick={() => {this.handleSaveClick()}}
+              >Save</IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+    
+        <IonContent>
+          <ListItems currentChecklist={this.props.currentChecklist} onItemClick={this.props.onItemClick}/>
+        </IonContent>
+      </>
+    );
+
+  }
+}
 
 const ListItems: React.SFC<ListProps> = ({currentChecklist, onItemClick}) => {
   const items = STONE_LIST.map(stone => {
@@ -45,7 +87,7 @@ const ListItems: React.SFC<ListProps> = ({currentChecklist, onItemClick}) => {
         <IonCheckbox 
           slot="start"
           checked={currentChecklist.foundStoneIds.some(i => i === stone.stoneId)}
-          onIonChange={(e: CustomEvent) => onItemClick(stone.stoneId)}
+          onIonChange={() => onItemClick(stone.stoneId)}
         />
         <IonLabel>
           <h2>{stone.locationName}</h2>

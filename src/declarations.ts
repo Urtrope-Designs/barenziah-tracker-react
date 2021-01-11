6b983b2a59805e80251e84c8cd7e1efd20f2191f
Sean <s.cunningham777@gmail.com>
2021-01-10 22:18:36 -0600

    #30 add stonePlacementDescription data

--- src/declarations.ts
+++ src/declarations.ts
@@ -15,6 +15,7 @@ export interface StoneLocationData {
   stoneId: number;
   locationName: string;
   sublocationName?: string;
+  stonePlacementDescription: string;
   holdName: 'Eastmarch'|'Falkreath'|'Haafingar'|'Hjaalmarch'|'The Reach'|'The Rift'|'Whiterun'|'Winterhold';
 }
 

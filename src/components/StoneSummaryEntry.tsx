f3be52b053deab44a1d73010348d40cabf702a54
Sean <s.cunningham777@gmail.com>
2021-01-14 07:32:43 -0600

    #27 #30 add stone detail expander

--- src/components/StoneSummaryEntry.tsx
+++ src/components/StoneSummaryEntry.tsx
@@ -1,7 +1,10 @@
 import { StoneLocation } from "../declarations";
-import { IonItem, IonCheckbox, IonLabel } from "@ionic/react";
-import React from "react";
+import { IonItem, IonCheckbox, IonLabel, IonButton, IonIcon } from "@ionic/react";
+import React, { useState } from "react";
 import { STONE_LIST } from "../util/stone-list";
+import { addOutline } from "ionicons/icons";
+
+import './StoneSummaryEntry.css';
 
 interface StoneSummaryEntryProps {
     stone: StoneLocation;
@@ -9,20 +12,33 @@ interface StoneSummaryEntryProps {
 }
 
 const StoneSummaryEntry: React.FC<StoneSummaryEntryProps> = ({ stone, setStoneFoundStatus }) => {
+    const [isDetailShown, setIsDetailShown] = useState(false);
     const stoneData = STONE_LIST.find(s => s.stoneId === stone.stoneId);
     return !!stoneData ? (
-        <IonItem>
+        <IonItem className="ion-align-items-start">
+            <IonLabel className="stoneSummary_headerLabel" onClick={(e) => {
+                    setIsDetailShown(!isDetailShown);
+                    e.stopPropagation();
+                }}
+            >
+                <h2>{stoneData.locationName}{!!stoneData.sublocationName ? ' - ' + stoneData.sublocationName : ''}</h2>
+                <p className="item-note">
+                    Hold: {stone.holdName}
+                </p>
+                {
+                    isDetailShown ? 
+                        <div className="ion-text-wrap">{stoneData.stonePlacementDescription}</div> :
+                        ''
+                }
+            </IonLabel>
             <IonCheckbox
                 slot="start"
                 checked={stone.isFound}
                 onIonChange={(e) => setStoneFoundStatus(stone.stoneId, e.detail.checked)}
             />
-            <IonLabel>
-                <h2>{stoneData.locationName}{!!stoneData.sublocationName ? ' - ' + stoneData.sublocationName : ''}</h2>
-                <p className="item-note">
-                    Hold: {stone.holdName}
-                </p>
-            </IonLabel>
+            <IonButton slot="end" fill="clear" onClick={(e) => {setIsDetailShown(!isDetailShown);}}>
+                <IonIcon slot="icon-only" icon={addOutline} className={'stoneSummary_showMoreButton' + (isDetailShown ? ' stoneSummary_showMoreButton-detailShown' : '')}/>
+            </IonButton>
         </IonItem>
     ) : null;
 }

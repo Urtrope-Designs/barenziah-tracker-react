abdb4000541ce1942e7bc30b71a04395e678d53b
Sean <s.cunningham777@gmail.com>
2021-01-18 15:31:40 -0600

    fix #27 fix #30 basic stone details section

--- src/components/StoneSummaryEntry.tsx
+++ src/components/StoneSummaryEntry.tsx
@@ -1,8 +1,8 @@
 import { StoneLocation } from "../declarations";
-import { IonItem, IonCheckbox, IonLabel, IonButton, IonIcon } from "@ionic/react";
-import React, { useState } from "react";
+import { IonItem, IonLabel, IonIcon, IonItemSliding, IonItemOptions, IonItemOption, } from "@ionic/react";
+import React, { useState, useRef } from "react";
 import { STONE_LIST } from "../util/stone-list";
-import { addOutline } from "ionicons/icons";
+import { addOutline, checkboxOutline, backspaceOutline } from "ionicons/icons";
 
 import './StoneSummaryEntry.css';
 
@@ -13,33 +13,42 @@ interface StoneSummaryEntryProps {
 
 const StoneSummaryEntry: React.FC<StoneSummaryEntryProps> = ({ stone, setStoneFoundStatus }) => {
     const [isDetailShown, setIsDetailShown] = useState(false);
+    const itemSlidingRef = useRef<HTMLIonItemSlidingElement | null>(null);
     const stoneData = STONE_LIST.find(s => s.stoneId === stone.stoneId);
+    const toggleStoneFoundStatus = () => {
+        setStoneFoundStatus(stone.stoneId, !stone.isFound);
+        itemSlidingRef.current?.close()
+    }
     return !!stoneData ? (
-        <IonItem className="ion-align-items-start">
-            <IonLabel className="stoneSummary_headerLabel" onClick={(e) => {
-                    setIsDetailShown(!isDetailShown);
-                    e.stopPropagation();
-                }}
-            >
-                <h2>{stoneData.locationName}{!!stoneData.sublocationName ? ' - ' + stoneData.sublocationName : ''}</h2>
-                <p className="item-note">
-                    Hold: {stone.holdName}
-                </p>
-                {
-                    isDetailShown ? 
-                        <div className="ion-text-wrap">{stoneData.stonePlacementDescription}</div> :
-                        ''
-                }
-            </IonLabel>
-            <IonCheckbox
-                slot="start"
-                checked={stone.isFound}
-                onIonChange={(e) => setStoneFoundStatus(stone.stoneId, e.detail.checked)}
-            />
-            <IonButton slot="end" fill="clear" onClick={(e) => {setIsDetailShown(!isDetailShown);}}>
-                <IonIcon slot="icon-only" icon={addOutline} className={'stoneSummary_showMoreButton' + (isDetailShown ? ' stoneSummary_showMoreButton-detailShown' : '')}/>
-            </IonButton>
-        </IonItem>
+        <div>
+            <IonItemSliding ref={itemSlidingRef}>
+                <IonItem color={(stone.isFound ? 'primary' : undefined)} lines={isDetailShown ? 'none' : 'full'} onClick={(e) => setIsDetailShown(!isDetailShown)}>
+                    <IonLabel className="stoneSummary_headerLabel">
+                        <h2>{stoneData.locationName}{!!stoneData.sublocationName ? ' - ' + stoneData.sublocationName : ''}</h2>
+                        <p className="item-note">
+                            Hold: {stone.holdName}
+                        </p>
+                    </IonLabel>
+                    <IonIcon slot="end" icon={addOutline} className={'stoneSummary_showMoreButton' + (isDetailShown ? ' stoneSummary_showMoreButton-detailShown' : '')}/>
+                </IonItem>
+                <IonItemOptions onIonSwipe={toggleStoneFoundStatus}>
+                    <IonItemOption expandable color={stone.isFound ? 'danger' : 'primary'} onClick={toggleStoneFoundStatus}>
+                        <IonIcon icon={stone.isFound ? backspaceOutline : checkboxOutline} slot="icon-only"></IonIcon>
+                    </IonItemOption>
+                </IonItemOptions>
+            </IonItemSliding>
+            {
+                isDetailShown
+                 ? (
+                    <div className="item-native stoneDetail_wrapper">
+                        <div className="stoneDetail_inner">
+                            {stoneData.stonePlacementDescription}
+                        </div>
+                    </div>
+                  )
+                 : ''
+            }
+        </div>
     ) : null;
 }
 

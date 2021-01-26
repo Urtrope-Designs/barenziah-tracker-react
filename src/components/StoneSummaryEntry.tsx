6d3ebaa3e40964aae99c35eec53d6dc367c5fa3a
Sean <s.cunningham777@gmail.com>
2021-01-25 21:48:28 -0600

    fix #37 with (max-)height transition :-/

--- src/components/StoneSummaryEntry.tsx
+++ src/components/StoneSummaryEntry.tsx
@@ -6,21 +6,10 @@ import { addOutline, checkboxOutline, backspaceOutline } from "ionicons/icons";
 
 import './StoneSummaryEntry.css';
 
-export interface toggleShowDetailCallbackContents {
-    element: HTMLElement;
-    content: HTMLElement;
-    shouldOpen: boolean;
-    startTransition: () => void;
-    endTransition: () => void;
-    setOpen: () => void;
-    setClosed: () => void;
-}
-
 interface StoneSummaryEntryProps {
     stone: StoneLocation;
     sortMode: 'groupByHold' | undefined;
     setStoneFoundStatus: (stoneId: number, value: boolean) => any;
-    toggleShowDetail: (contents: toggleShowDetailCallbackContents) => void;
 }
 
 const buildLabelGroupedByHold = (stoneData: StoneLocationData) => {
@@ -44,47 +33,19 @@ const buildLabel = (stoneData: StoneLocationData) => {
     );
 }
 
-const StoneSummaryEntry: React.FC<StoneSummaryEntryProps> = ({ stone, sortMode, setStoneFoundStatus, toggleShowDetail }) => {
+const StoneSummaryEntry: React.FC<StoneSummaryEntryProps> = ({ stone, sortMode, setStoneFoundStatus }) => {
     const [isDetailShown, setIsDetailShown] = useState(false);
-    const [isTransitioning, setIsTransitioning] = useState(false);
     const itemSlidingRef = useRef<HTMLIonItemSlidingElement | null>(null);
-    const hostRef = useRef<HTMLDivElement | null>(null);
-    const contentRef = useRef<HTMLDivElement | null>(null);
+    const stoneDetailInnerRef = useRef<HTMLDivElement | null>(null);
     const stoneData = STONE_LIST.find(s => s.stoneId === stone.stoneId);
     const toggleStoneFoundStatus = () => {
         setStoneFoundStatus(stone.stoneId, !stone.isFound);
         itemSlidingRef.current?.close()
     }
-    const itemClickHandler = () => {
-        if (isTransitioning || !hostRef.current || !contentRef.current) {
-            return;
-        }
-        const shouldOpen = !isDetailShown;
-        // setIsDetailShown(shouldOpen);
-        // setIsTransitioning(true);
-
-        toggleShowDetail({
-            element: hostRef.current,
-            content: contentRef.current,
-            shouldOpen: shouldOpen,
-            startTransition: () => {
-                setIsTransitioning(true);
-            },
-            endTransition: () => {
-                setIsTransitioning(false);
-            },
-            setOpen: () => {
-                setIsDetailShown(true);
-            },
-            setClosed: () => {
-                setIsDetailShown(false);
-            }
-        })
-    }
     return !!stoneData ? (
-        <div ref={hostRef} className="stoneSummary_root">
+        <div style={{overflow: 'hidden'}}>
             <IonItemSliding ref={itemSlidingRef}>
-                <IonItem color={(stone.isFound ? 'primary' : undefined)} lines={isDetailShown ? 'none' : 'full'} onClick={itemClickHandler}>
+                <IonItem color={(stone.isFound ? 'primary' : undefined)} lines={isDetailShown ? 'none' : 'full'} onClick={(e) => setIsDetailShown(!isDetailShown)}>
                     <IonLabel className="stoneSummary_headerLabel">
                         {
                             sortMode === 'groupByHold'
@@ -100,8 +61,8 @@ const StoneSummaryEntry: React.FC<StoneSummaryEntryProps> = ({ stone, sortMode, 
                     </IonItemOption>
                 </IonItemOptions>
             </IonItemSliding>
-            <div ref={contentRef} className="item-native stoneDetail_wrapper">
-                <div className="stoneDetail_inner">
+            <div className={`item-native stoneDetail_wrapper${isDetailShown ? ' stoneDetail_wrapper-detailShown' : ''}`} style={{['--stoneDetail-innerHeight' as any]: `${stoneDetailInnerRef.current?.clientHeight}px`}}>
+                <div className="stoneDetail_inner" ref={stoneDetailInnerRef}>
                     {stoneData.stonePlacementDescription}
                 </div>
             </div>
--- src/components/StoneSummaryList.tsx
+++ src/components/StoneSummaryList.tsx
@@ -1,9 +1,7 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { StoneLocation } from "../declarations";
-import StoneSummaryEntry, { toggleShowDetailCallbackContents } from "./StoneSummaryEntry";
-import { IonList, IonItemDivider, IonLabel, createAnimation, Animation } from '@ionic/react';
-
-import './StoneSummaryList.css';
+import StoneSummaryEntry from "./StoneSummaryEntry";
+import { IonList, IonItemDivider, IonLabel } from '@ionic/react';
 
 interface StoneSummaryListProps {
     stoneLocations: StoneLocation[];
@@ -11,7 +9,7 @@ interface StoneSummaryListProps {
     setStoneFoundStatus(stoneId: number, value: boolean): any;
 }
 
-const groupStonesByHold = (stoneLocations: StoneLocation[], setStoneFoundStatus: (stoneId: number, value: boolean) => any, toggleShowDetail: (contents: toggleShowDetailCallbackContents) => void) => {
+const groupStonesByHold = (stoneLocations: StoneLocation[], setStoneFoundStatus: (stoneId: number, value: boolean) => any) => {
     const groupedStones: {holdName: string, stones: StoneLocation[]}[] = [];
     stoneLocations.forEach((next: StoneLocation) => {
         const existingGroup = groupedStones.find(group => group.holdName === next.holdName);
@@ -42,179 +40,27 @@ const groupStonesByHold = (stoneLocations: StoneLocation[], setStoneFoundStatus:
         return [
             <IonItemDivider key={group.holdName}><IonLabel>Hold: {group.holdName}</IonLabel></IonItemDivider>,
             group.stones.map(stone => {
-                return <StoneSummaryEntry key={stone.stoneId} stone={stone} setStoneFoundStatus={setStoneFoundStatus} sortMode='groupByHold' toggleShowDetail={toggleShowDetail}/>;
+                return <StoneSummaryEntry key={stone.stoneId} stone={stone} setStoneFoundStatus={setStoneFoundStatus} sortMode='groupByHold'/>;
             })
         ];
     })
 }
 
-const unsort = (stoneLocations: StoneLocation[], setStoneFoundStatus: (stoneId: number, value: boolean) => any, toggleShowDetail: (contents: toggleShowDetailCallbackContents) => void) => {
+const unsort = (stoneLocations: StoneLocation[], setStoneFoundStatus: (stoneId: number, value: boolean) => any) => {
     return stoneLocations.map(stonLoc => {
-        return <StoneSummaryEntry key={stonLoc.stoneId} stone={stonLoc} setStoneFoundStatus={setStoneFoundStatus} sortMode={undefined} toggleShowDetail={toggleShowDetail}/>
+        return <StoneSummaryEntry key={stonLoc.stoneId} stone={stonLoc} setStoneFoundStatus={setStoneFoundStatus} sortMode={undefined}/>
     });
 }
 
 
 const StoneSummaryList: React.FC<StoneSummaryListProps> = ({stoneLocations, sortMode, setStoneFoundStatus}) => {
-    const hostRef = useRef<HTMLIonListElement | null>(null);
-    const blockerRef = useRef<HTMLDivElement | null>(null);
-    let elementsToShift: Array<any>;
-    let currentlyOpen: toggleShowDetailCallbackContents | null = null;
-
-    let shiftDownAnimation: Animation;
-    let blockerDownAnimation: Animation;
-
-    const toggleShowDetail = async (contents: toggleShowDetailCallbackContents) => {
-        if (!hostRef.current || !blockerRef.current) {
-            return;
-        }
-
-        contents.startTransition();
-        contents.shouldOpen ? await animateOpen(contents) : await animateClose(contents);
-        contents.endTransition();
-    }
-
-    const closeOpenItem = async () => {
-        if (currentlyOpen !== null) {
-            const itemToClose = currentlyOpen;
-        
-            itemToClose.startTransition();
-            await animateClose(currentlyOpen);
-            itemToClose.endTransition();
-            return true;
-        }
-    }
-
-    const animateOpen = async (contents: toggleShowDetailCallbackContents) => {
-        if (!hostRef.current || !blockerRef.current) {
-            return;
-        }
-
-        // Close any open item first
-        await closeOpenItem();
-        currentlyOpen = contents;
-
-        contents.setOpen();
-
-        // Create an array of all accordion items
-        const items = Array.from(hostRef.current.children);
-
-        // Find the item being opened, and create a new array with only the elements beneath the element being opened
-        let splitOnIndex = 0;
-
-        items.forEach((item, index) => {
-            if (item === contents.element) {
-                splitOnIndex = index;
-            }
-        });
-
-        elementsToShift = [...items].splice(splitOnIndex + 1, items.length - (splitOnIndex + 1));
-
-        // Set item content to be visible
-        contents.content.style.display = 'block';
-
-        // Calculate the amount other items need to be shifted
-        const amountToShift = contents.content.clientHeight;
-        const openAnimationTime = 400;
-
-        // Initially set all items below the one being opened to cover the new content
-        // but then animate back to their normal position to reveal the content
-        shiftDownAnimation = createAnimation()
-            .addElement(elementsToShift)
-            .delay(20)
-            .beforeStyles({
-                'transform': `translateY(-${amountToShift}px)`,
-                'position': 'relative',
-                'z-index': '1',
-            })
-            .afterClearStyles(['position', 'z-index'])
-            .to('transform', 'translateY(0)')
-            .duration(openAnimationTime)
-            // .easing('cubic-bezier(0.32,0.72,0,1)');
-            .easing('ease-out');
-
-        // This blocker element is placed after the last item in the accordion list
-        // It will change its height to the height of the content being displayed so that
-        // the content doesn't leak out the bottom of the list
-        blockerDownAnimation = createAnimation()
-            .addElement(blockerRef.current)
-            .delay(20)
-            .beforeStyles({
-                'transform': `translateY(-${amountToShift}px)`,
-                'height': `${amountToShift}px`,
-            })
-            .to('transform', 'translateY(0)')
-            .duration(openAnimationTime)
-            // .easing('cubic-bezier(0.32,0.72,0,1)');
-            .easing('ease-out');
-
-        return await Promise.all([shiftDownAnimation.play(), blockerDownAnimation.play()]);
-    };
-
-    const animateClose = async (contents: toggleShowDetailCallbackContents) => {
-        if (!hostRef.current || !blockerRef.current) {
-            return;
-        }
-
-        currentlyOpen = null;
-        contents.setClosed();
-        const amountToShift = contents.content.clientHeight;
-
-        const closeAnimationTime = 400;
-
-        // Now we first animate up the elements beneath the content that was opened to cover it
-        // and then we set the content back to display: none and remove the transform completely
-        // With the content gone, there will be no noticeable position change when removing the transform
-        const shiftUpAnimation: Animation = createAnimation()
-            .addElement(elementsToShift)
-            .afterStyles({
-                'transform': 'translateY(0)',
-            })
-            .to('transform', `translateY(-${amountToShift}px)`)
-            .afterAddWrite(() => {
-                shiftDownAnimation.destroy();
-                blockerDownAnimation.destroy();
-            })
-            .duration(closeAnimationTime)
-            // .easing('cubic-bezier(0.32,0.72,0,1)');
-            .easing('ease-out');
-
-        const blockerUpAnimation: Animation = createAnimation()
-            .addElement(blockerRef.current)
-            .afterStyles({
-                'transform': 'translateY(0)',
-            })
-            .to('transform', `translateY(-${amountToShift}px)`)
-            .duration(closeAnimationTime)
-            // .easing('cubic-bezier(0.32,0.72,0,1)');
-            .easing('ease-out');
-
-        elementsToShift.length
-            ? await Promise.all([shiftUpAnimation.play(), blockerUpAnimation.play()])
-            : await blockerUpAnimation.play();
-        
-        // Hide the content again
-        contents.content.style.display = 'none';
-
-        // Destroy the animations to reset the CSS values that they applied. This will remove the transforms instantly.
-        shiftUpAnimation.destroy();
-        blockerUpAnimation.destroy();
-
-        return true;
-    };
-
-    return (
-        <React.Fragment>
-            <IonList ref={hostRef} className="stoneSummaryList">
+    return <IonList>
             {
                 sortMode === 'groupByHold' 
-                ? groupStonesByHold(stoneLocations, setStoneFoundStatus, toggleShowDetail)
-                : unsort(stoneLocations, setStoneFoundStatus, toggleShowDetail)
+                ? groupStonesByHold(stoneLocations, setStoneFoundStatus)
+                : unsort(stoneLocations, setStoneFoundStatus)
             }
-            </IonList>
-            <div ref={blockerRef} className="stoneSummaryList_blocker"></div>
-        </React.Fragment>
-    );
+            </IonList>;
 }
 
 export default StoneSummaryList;

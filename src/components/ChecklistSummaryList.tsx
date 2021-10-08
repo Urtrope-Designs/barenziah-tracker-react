f07594b9c68bb4460186f9d37e61644359ca6876
Sean <s.cunningham777@gmail.com>
2021-10-07 21:21:17 -0500

    #64 links to app store listings for ratings

--- src/components/ChecklistSummaryList.tsx
+++ src/components/ChecklistSummaryList.tsx
@@ -20,6 +20,7 @@ import {
   IonPage,
 } from '@ionic/react';
 import { trash, close } from 'ionicons/icons';
+import { Capacitor } from '@capacitor/core';
 import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { ChecklistSummary } from '../declarations';
@@ -157,7 +158,12 @@ class ChecklistSummaryList extends React.Component<ChecklistSummaryListProps, Ch
             <IonPage>
               <div>
                 <h1>All about it</h1>
-                <p>This app was lovingly crafted by <a href="https://urtropedesigns.com/" target="_blank" rel="noreferrer noopener">Urtrope Designs</a></p>
+                <p>This app was lovingly crafted by <a href="https://urtropedesigns.com/" target="_blank" rel="noreferrer noopener">Urtrope Designs</a>. 
+                {Capacitor.isNativePlatform() 
+                  ? <span>If you find it rad or at least mildly useful, please <a href={Capacitor.getPlatform() === "ios" ? "https://apps.apple.com/us/app/barenziah-tracker/id1585514338" : "https://play.google.com/store/apps/details?id=com.urtropedesigns.barenziahtracker"} target="_blank" rel="noreferrer noopener">rate or review it</a>!</span>
+                  : ''
+                }
+                </p>
                 <p>Most of the content in this app was pulled from the "Stones of Barenziah" article on the <a href="https://elderscrolls.fandom.com/wiki/Stones_of_Barenziah" target="_blank" rel="noreferrer noopener">Elder Scrolls Fandom Wiki</a></p>
                 <p>As for the email address you used to log in, I'm seriously not going to do anything with it beyond the purposes of handling your log-ins, but you can read the full legalese here in my <a href="https://www.termsfeed.com/live/ae0253cc-690a-43e6-9961-26964c15b6eb" target="_blank" rel="noreferrer noopener">privacy policy</a></p>
               </div>

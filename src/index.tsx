d6f80038172b68fe92e61123ed2540228c307cf5
Sean <s.cunningham777@gmail.com>
2021-10-07 21:35:03 -0500

    #62 splash screen config & such

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import BtrApp from './BtrApp';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar } from '@capacitor/status-bar';
+import { Capacitor } from '@capacitor/core';
 
 // // Use matchMedia to check the user preference
 // const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
@@ -15,6 +17,12 @@ toggleDarkTheme(true);
 // Add or remove the "dark" class based on if the media query matches
 function toggleDarkTheme(shouldAdd: boolean) {
   document.body.classList.toggle('dark', shouldAdd);
+}
+
+if (Capacitor.isNativePlatform()) {
+  StatusBar.hide()
+     .catch(console.log);
+  SplashScreen.hide();
 }
 
 ReactDOM.render(<BtrApp />, document.getElementById('btr_app'));
